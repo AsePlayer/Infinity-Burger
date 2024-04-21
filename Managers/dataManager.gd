b521c443b6e9dca0379775bd93694c8e1155035c
AsePlayer <52973701+AsePlayer@users.noreply.github.com>
2024-04-21 11:19:44 -0700

    No save file makes new one. Populated points to screen

--- Managers/dataManager.gd
+++ Managers/dataManager.gd
@@ -3,9 +3,13 @@ extends Node2D
 var json = JSON.new()
 var path = "res://data.json"
 
+@onready var buttons = $"../../Tablet/Screen/GameScreen/Buttons"
+
 var default_data = {
 	"money": 0,
 	"screen_color": 0,
+	"music": "default",
+	"name": "BurgerBoi",
 	"screen_colors": [[0, false, 100],[1, false, 200],[2, false, 500],[3, false, 1000],[4, false, 1000],[5, false, 1000]]	
 }
 # Player's actual data
@@ -14,16 +18,19 @@ var save_data = default_data
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	load_game()
-	modify_data("money", int(save_data["money"]) + 5)
-	load_game()
 	pass # Replace with function body.
 
 
 func save_game():
 	var file = FileAccess.open(path, FileAccess.WRITE)
+	save_data["money"] += buttons.points
+	print("added this many points to save: " + str(buttons.points))
+	buttons.points = 0
+	buttons.dollars_for_burger.text = "[center]%s[/center]" % ("$0")
 	file.store_line(json.stringify(save_data, "\t"))
 	file.close()
 	file = null
+	load_game()
 
 
 func load_game():
@@ -34,6 +41,8 @@ func load_game():
 		
 	var content = json.parse_string(file.get_as_text())
 	save_data = content
+	
+	buttons.dollars_total.text = "[center]%s[/center]" % ("$" + str(save_data["money"]))
 	print(save_data)
 	return content
 	
